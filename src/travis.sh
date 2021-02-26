6109fdbb6b6e6273c4063e5be80791b3ba94c26e
Andrea Telatin <andrea@telatin.com>
2021-02-26 13:41:55 +0000

    update-ci

--- src/travis.sh
+++ src/travis.sh
@@ -1,5 +1,5 @@
+set -euxo pipefail
 export CHOOSENIM_CHOOSE_VERSION=1.2.10
-apt install -y zlib1g-dev wget curl build-essential
 # wget -qO - https://nim-lang.org/choosenim/init.sh | sh
 curl https://nim-lang.org/choosenim/init.sh -sSf > init.sh
 sh init.sh -y
