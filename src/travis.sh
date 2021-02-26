0ec0d58b59d349d8afef5f92941bd486beb7e796
Andrea Telatin <andrea@telatin.com>
2021-02-26 13:52:02 +0000

    fix-path-home

--- src/travis.sh
+++ src/travis.sh
@@ -4,6 +4,7 @@ export CHOOSENIM_CHOOSE_VERSION=1.2.10
 curl https://nim-lang.org/choosenim/init.sh -sSf > init.sh
 sh init.sh -y
 rm init.sh
+export PATH=/home/travis/.nimble/bin:$HOME/.nimble/bin:$PATH
 nimble refresh
 nimble build -y  --verbose
 bash test/mini.sh
