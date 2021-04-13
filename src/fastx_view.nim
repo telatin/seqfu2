7daf40921a0c888084b27cf5e1879d70f3a5880e
Andrea Telatin <andrea@telatin.com>
2021-04-13 11:17:41 +0000

    view-qual-update

--- src/fastx_view.nim
+++ src/fastx_view.nim
@@ -4,6 +4,55 @@ from os import fileExists
 import docopt
 import ./seqfu_utils
 import colorize
+import encodings
+
+
+proc qualToAscii*(s: string, v: seq[int], color = false): string =
+  #  1   2   3   4   5   6   7
+  #  Low     Mid     Ok
+  for i in s:
+    let
+      val = charToQual(i)
+    if val <= v[0]:
+      if color == true:
+        result &= "x".fgDarkGray
+      else:
+        result &= "x"
+    elif val <= v[1]:
+      if color == true:
+        result &= "_".fgDarkGray
+      else:
+        result &= "_"
+    elif val <= v[2]:
+      if color == true:
+        result &= "_".fgRed
+      else:
+        result &= "_"
+    elif val <= v[3]:
+      if color == true:
+        result &= "o".fgRed
+      else:
+        result &= "o"
+    elif val <= v[4]:
+      if color == true:
+        result &= "o".fgYellow
+      else:
+        result &= "o"
+    elif val <= v[5]:
+      if color == true:
+        result &= "i".fgYellow
+      else:
+        result &= "i"
+    elif val <= v[6]:
+      if color == true:
+        result &= "i".fgGreen
+      else:
+        result &= "i"
+    else:
+      if color == true:
+        result &= "I".fgGreen
+      else:
+        result &= "I"
 
 
 proc qualToUnicode*(s: string, v: seq[int], color = false): string =
@@ -14,9 +63,9 @@ proc qualToUnicode*(s: string, v: seq[int], color = false): string =
       val = charToQual(i)
     if val <= v[0]:
       if color == true:
-        result &= "▁".fgDarkGray
+        result &= "_".fgDarkGray
       else:
-        result &= "▁"
+        result &= "_"
     elif val <= v[1]:
       if color == true:
         result &= "▂".fgDarkGray
@@ -52,7 +101,6 @@ proc qualToUnicode*(s: string, v: seq[int], color = false): string =
         result &= "█".fgGreen
       else:
         result &= "█"
-
 
 proc qualToColor(s: string, v: seq[int], color = false): string =
   #  1   2   3   4   5   6   7
@@ -158,7 +206,8 @@ Options:
   --match-ths FLOAT      Oligo matching threshold [default: 0.75]
   --min-matches INT      Oligo minimum matches [default: 5]
   --max-mismatches INT   Oligo maxmimum mismataches [default: 2]
-
+  --ascii                Encode the quality as ASCII chars (when UNICODE is
+                         not available)
   -Q, --qual-chars       Show quality characters instead of bars
   -n, --nocolor          Disable colored output
   --verbose              Show extra information
@@ -168,6 +217,7 @@ Options:
 
   verbose = args["--verbose"]
   let
+    ce = getCurrentEncoding()
     colorOutput = not args["--nocolor"]
     matchThs = parseFloat($args["--match-ths"])
     minMatches = parseInt($args["--min-matches"])
@@ -223,9 +273,13 @@ Options:
     if args["--qual-chars"]:
       echo qualToColor(read.qual, thresholdValues, colorOutput)
     else:
-      echo qualToUnicode(read.qual, thresholdValues, colorOutput)
+      if args["--ascii"]:
+        echo qualToAscii(read.qual, thresholdValues, colorOutput)
+      else:
+        echo qualToUnicode(read.qual, thresholdValues, colorOutput)
 
-     
+  if args["--verbose"]:
+    stderr.writeLine("Encoding: ", ce)     
   return 0
 
 
