a76ad2aabbe27b24728e6fb730bcc901f122cc36
Andrea Telatin (QIB) <telatina@n85984.nbi.ac.uk>
2021-10-22 09:35:39 +0100

    fix-cov

--- src/fu_cov.nim
+++ src/fu_cov.nim
@@ -8,9 +8,8 @@ import os
 import algorithm
 import ./seqfu_utils
 
-const NimblePkgVersion {.strdefine.} = "undef"
-const programVersion = if NimblePkgVersion == "undef": "X.9"
-                       else: NimblePkgVersion
+const NimblePkgVersion {.strdefine.} = "pre-release"
+
 let
   programName = "fu-cov"
 #[
@@ -43,23 +42,8 @@ proc rev[T](xs: openarray[T]): seq[T] =
     #result[^i-1] = x # or:
     result[xs.high-i] = x
 
-proc verbose(msg: string, print: bool) =
-  if print:
-    stderr.writeLine(" - ", msg)
-
-
-proc format_dna(seq: string, format_width: int): string =
-  if format_width == 0:
-    return seq
-  for i in countup(0,seq.len - 1,format_width):
-    #let endPos = if (seq.len - i < format_width): seq.len - 1
-    #            else: i + format_width - 1
-    if (seq.len - i <= format_width):
-      result &= seq[i..seq.len - 1]
-    else:
-      result &= seq[i..i + format_width - 1] & "\n"
-
-
+ 
+ 
 proc getNumberAfterPos(s: string, pos: int): float =
   var ans = ""
   for i in pos .. s.high:
@@ -86,7 +70,7 @@ proc main(args: var seq[string]): int =
   Extract contigs using coverage data from the assembler
 
   Usage: 
-  fu-cov [options] [inputfile ...]
+  fu-cov [options] [<assembly-file>...]
 
   Options:
     -c, --min-cov FLOAT    Minimum coverage [default: 0.0]
@@ -97,28 +81,61 @@ proc main(args: var seq[string]): int =
     -s, --sort             Store contigs in memory and sort them by descending coverage
     --verbose              Print verbose log
     --help                 Show help
-  """, version=programVersion, argv=commandLineParams())
+  """, version=NimblePkgVersion, argv=commandLineParams())
+
+  var
+    skip_hi_cov = 0
+    skip_lo_cov = 0
+    skip_short  = 0
+    skip_long   = 0
+    bases_printed = 0
+    total_bases = 0
+    covTable    = newSeq[ContigInfo]()
+
+    optmincov: float
+    optmaxcov: float
+    optminlen: int
+    optmaxlen: int
+    printtop: int
+
   try:
-    var
-      skip_hi_cov = 0
-      skip_lo_cov = 0
-      skip_short  = 0
-      skip_long   = 0
-      covTable    = newSeq[ContigInfo]()
+    optmincov = parseFloat($args["--min-cov"])
+    optmaxcov = parseFloat($args["--max-cov"])
+  except:
+    stderr.writeLine("Error: invalid arguments. Minimum and maximum coverage must be float: ", $args["--min-cov"], ",", $args["--max-cov"])
+    return 1
+
+  try:
+    optminlen = parseInt($args["--min-len"])
+    optmaxlen = parseInt($args["--max-len"])
+    printtop    = parseInt($args["--top"])
+  except:
+    stderr.writeLine("Error: invalid arguments. Minimum and maximum len must be int: ", $args["--min-len"], ",", $args["--max-len"])
+    return 1
+
+  try:
+    discard len(@(args["<assembly-file>"]))
+  except Exception as e:
+    stderr.writeLine("Error: parsing files. ", e.msg)
+    return 1
+    
+
+  try:
 
 
 
-    if len(args["inputfile"]) == 0:
+    if len(@(args["<assembly-file>"])) == 0:
       echo "Missing argument: input file (type -h for more info)"
       #if not $args["help == true:
       #  echo "Type --help for more info."
       quit(0)
 
+  
     var
       c  = 0      # total count of sequences
       pf = 0      # passing filters
       ff = 0      # parsed files
-    for filename in args["inputfile"]:
+    for filename in args["<assembly-file>"]:
       if not fileExists(filename):
         echo "FATAL ERROR: File ", filename, " not found."
         quit(1)
@@ -126,56 +143,62 @@ proc main(args: var seq[string]): int =
       var f = xopen[GzFile](filename)
       defer: f.close()
       var r: FastxRecord
-      verbose("Reading " & filename, args["verbose"])
+      if args["--verbose"]:
+        stderr.writeLine "Reading ", filename
+
       ff += 1
       # Prse FASTX
-      var match: array[1, string]
+      #var match: array[1, string]
 
 
       while f.readFastx(r):
         c+=1
+        total_bases += len(r.seq)
         lenStats.push(len(r.seq))
         var cov = getCovFromString(r.name & " " & r.comment)
 
         # Coverage check
         if cov >= 0:
           covStats.push(cov)
-          if $args["min_cov"] != "0.0" and cov < parseFloat($args["min_cov"]):
+          if optmincov != 0.0 and cov < optmincov:
             skip_lo_cov += 1
             continue
-          if $args["max_cov"] != "0.0" and cov > parseFloat($args["max_cov"]):
+          if optmaxcov != 0.0 and cov > optmaxcov:
             skip_hi_cov += 1
             continue
 
         # Contig length filter
-        if $args["min_len"] != "0" and len(r.seq) < parseInt($args["min_len"]):
+        if optminlen > 0 and len(r.seq) < optminlen:
           skip_short += 1
           continue
-        if $args["max_len"] != "0" and len(r.seq) > parseInt($args["max_len"]):
+        if optmaxlen > 0 and len(r.seq) > optmaxlen:
           skip_long += 1
           continue
 
 
         pf += 1
-
-        if args["sort"] == false:
+        bases_printed += len(r.seq)
+        if args["--sort"] == false:
           echo ">", r.name, " ", r.comment, "\n", r.seq;
         else:
           covTable.add((name: r.name, comment: r.comment, cov: cov, length: len(r.seq), sequence: r.seq))
+    let
+      ratio = 100.0 * float(bases_printed) / float(total_bases)
 
     stderr.writeLine(pf, "/", lenStats.n, " sequences printed (", covStats.n ," with coverage info) from ", ff , " files.")
     stderr.writeLine(fmt"Skipped:          {skip_short} too short, {skip_long} too long, then {skip_lo_cov} low coverage, {skip_hi_cov} high coverage, .")
+    stderr.writeLine(fmt"Total size:       {bases_printed}/{total_bases} bp printed ({ratio:.1f}%)")
     stderr.writeLine(fmt"Average length:   {lenStats.mean():.2f} bp, [{lenStats.min} - {lenStats.max}]")
     if covStats.n > 0:
       stderr.writeLine(fmt"Average coverage: {covStats.mean():.2f}X, [{covStats.min:.1f}-{covStats.max:.1f}]")
 
-    if args["sort"] == true:
+    if args["--sort"] == true:
       var
         top = 0
       for i in rev(covTable.sortedByIt(it.cov)):
         top += 1
 
-        if $args["top"] != "0" and top > parseInt($args["top"]):
+        if printtop != 0 and top > printtop:
           break
 
         echo ">", i.name, " ", i.comment, "\n", i.sequence
