506f93e9c2cb3397a4e3b657a8995b1e6ccefe49
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2022-07-07 22:28:46 +0100

    Update splash.sh

--- test/splash.sh
+++ test/splash.sh
@@ -1,6 +1,8 @@
-
 #!/bin/bash
-
+set -euxo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+ldd $SCRIPT_DIR/../bin/seqfu
+echo $SHELL
+whoami
+file $SCRIPT_DIR/../bin/seqfu
 $SCRIPT_DIR/../bin/seqfu
