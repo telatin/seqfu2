f8b624eda140a68925c86b0a922d996d61df57b9
AT! <15690844+telatin@users.noreply.github.com>
2025-12-23 16:50:54 +0000

    add tofasta

--- src/sfu.nim
+++ src/sfu.nim
@@ -30,7 +30,7 @@ include ./fastx_metadata
 include ./fastx_tabulate
 include ./fastx_check
 include ./fu_rotate
-
+include ./tofasta
 
 # Experimental
 include ./fastx_stats_v2
@@ -80,6 +80,7 @@ var progs = {
         "rot": fastx_rotate,
         "restart": fastx_rotate,
       "check": fqcheck,
+      "tofasta": tofasta
 }.toTable
 
 proc main(args: var seq[string]): int =
@@ -98,7 +99,8 @@ proc main(args: var seq[string]): int =
                "sort [srt]"        : "sort sequences by size (uniques)",
                "metadata [met]"    : "print a table of FASTQ reads (mapping files)",
                "list [lst]"        : "print sequences from a list of names",
-               }.toTable
+               "tofasta"           : "convert multiple formats to FASTA"
+            }.toTable
 
     helps_last = {"cat"            : "concatenate FASTA/FASTQ files",
                   "head"           : "print first sequences",
